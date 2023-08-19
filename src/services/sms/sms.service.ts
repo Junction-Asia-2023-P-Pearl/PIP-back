e6a0b33b4da1f581344e8ceb4c1dd8710e996f9a
jasper200207 <jasper12394@gmail.com>
2023-08-20 04:29:10 +0900

    sms msg

--- src/services/sms/sms.service.ts
+++ src/services/sms/sms.service.ts
@@ -30,7 +30,7 @@ export class SmsService {
     const medicine = await this.medicineRepository.findOne({
       where: { _id: medicineId },
     });
-    const message = `${patient.name}님에게 ${medicine.name}mg을 ${amount}만큼 투여했습니다.`;
+    const message = `${patient.name}님에게 ${medicine.name}을 ${amount}mg 만큼 투여했습니다.`;
     await sendMsg(message, [patient.guardian.phoneNumber]);
   }
 }
